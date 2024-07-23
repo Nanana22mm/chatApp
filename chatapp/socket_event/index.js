13c806c29c6903477a2ad4ac48b61fe9ea7af3a9
Nanana22mm <3110nanana2002@sslab.ics.keio.ac.jp>
2024-07-23 18:57:17 +0900

    send msg

--- chatapp/socket_event/index.js
+++ chatapp/socket_event/index.js
@@ -1,16 +1,21 @@
 export default (io, socket) => {
   // 入室メッセージをクライアントに送信する
   socket.on("enterEvent", (data) => {
-    socket.broadcast.emit("enterEvent", data)
+    io.sockets.emit("enterEvent", data)
   })
 
   // 退室メッセージをクライアントに送信する
   socket.on("exitEvent", (data) => {
-    socket.broadcast.emit("exitEvent", data)
+    io.sockets.emit("exitEvent", data)
   })
 
   // 投稿メッセージを送信する
   socket.on("publishEvent", (data) => {
     io.sockets.emit("publishEvent", data)
+  })
+
+  // memo を送信する
+  socket.on("publishMemo", (data) => {
+    io.sockets.emit("publishMemo", data)
   })
 }
