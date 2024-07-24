f2ec4b02cbc83d198e23277adff583e68794329a
ichikawa01 <ryo.i.343826@gmail.com>
2024-07-24 15:02:43 +0900

    入室と退室の表示

--- chatapp/src/components/Chat.vue
+++ chatapp/src/components/Chat.vue
@@ -31,7 +31,7 @@ const onPublish = () => {
 
 // 退室メッセージをサーバに送信する
 const onExit = () => {
-
+  socket.emit("exitEvent", userName.value)
 }
 
 // メモを画面上に表示する
@@ -46,12 +46,12 @@ const onMemo = () => {
 // #region socket event handler
 // サーバから受信した入室メッセージ画面上に表示する
 const onReceiveEnter = (data) => {
-  chatList.push()
+  chatList.unshift(`${data}さんが入室しました。`)
 }
 
 // サーバから受信した退室メッセージを受け取り画面上に表示する
 const onReceiveExit = (data) => {
-  chatList.push()
+  chatList.unshift(`${data}さんが退室しました。`)
 }
 
 // サーバから受信した投稿メッセージを画面上に表示する
@@ -65,12 +65,12 @@ const onReceivePublish = (data) => {
 const registerSocketEvent = () => {
   // 入室イベントを受け取ったら実行
   socket.on("enterEvent", (data) => {
-
+    onReceiveEnter(data)
   })
 
   // 退室イベントを受け取ったら実行
   socket.on("exitEvent", (data) => {
-
+    onReceiveExit(data)
   })
 
   // 投稿イベントを受け取ったら実行
