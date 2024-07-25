b7f0b04381cffa5d6bc58d36feb79054fc0ebcde
Nanana22mm <3110nanana2002@sslab.ics.keio.ac.jp>
2024-07-25 16:47:54 +0900

    入室，退出表示

--- chatapp/socket_event/index.js
+++ chatapp/socket_event/index.js
@@ -1,12 +1,12 @@
 export default (io, socket) => {
   // 入室メッセージをクライアントに送信する
   socket.on("enterEvent", (data) => {
-    io.sockets.emit("enterEvent", data)
+    socket.broadcast.emit("enterEvent", data)
   })
 
   // 退室メッセージをクライアントに送信する
   socket.on("exitEvent", (data) => {
-    io.sockets.emit("exitEvent", data)
+    socket.broadcast.emit("exitEvent", data)
   })
 
   // 投稿メッセージを送信する
