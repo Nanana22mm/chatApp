dbfd3ede03da56d62cd774ca19b9ceba3f30299b
harukif <harukif0414@gmail.com>
2024-07-30 13:43:23 +0900

    add button to toukou

--- chatapp/src/components/Chat.vue
+++ chatapp/src/components/Chat.vue
@@ -52,11 +52,30 @@ const onMemo = () => {
   const chatTime = new Date()
   var Time = chatTime.getFullYear() + '/' + ('0' + (chatTime.getMonth() + 1)).slice(-2) + '/' +('0' + chatTime.getDate()).slice(-2) + ' ' +  ('0' + chatTime.getHours()).slice(-2) + ':' + ('0' + chatTime.getMinutes()).slice(-2);
 
-  // メモの内容を表示
-  chatList.unshift(`${userName.value}さんのメモ [${Time}]: ` + chatContent.value)
+  // // メモの内容を表示
+  // chatList.unshift(`${userName.value}さんのメモ [${Time}]: ` + chatContent.value)
+  chatList.unshift({
+    time: Time,
+    user: userName.value,
+    content: chatContent.value,
+    type: "memo"
+  })
 
   // 入力欄を初期化
   chatContent.value = ""
+}
+
+// メモを削除する
+const onDeleteMemo = (index) => {
+  chatList.splice(index, 1)
+}
+
+// メモを編集する
+const onEditMemo = (index) => {
+  const newContent = prompt("メモを編集してください：", chatList[index].content)
+  if (newContent !== null && newContent !== "") {
+    chatList[index].content = newContent
+  }
 }
 // #endregion
 
@@ -73,7 +92,26 @@ const onReceiveExit = (data) => {
 
 // サーバから受信した投稿メッセージを画面上に表示する
 const onReceivePublish = (time, name, data) => {
-  chatList.unshift(`${name}さんの投稿 [${time}]: ${data}`)
+  // chatList.unshift(`${name}さんの投稿 [${time}]: ${data}`)
+  chatList.unshift({
+    time: time,
+    user: name,
+    content: data,
+    type: "publish"
+  })
+}
+
+// 投稿を削除する
+const onDeletePublish = (index) => {
+  chatList.splice(index, 1)
+}
+
+// 投稿を編集する
+const onEditPublish = (index) => {
+  const newContent = prompt("投稿を編集してください：", chatList[index].content)
+  if (newContent !== null && newContent !== "") {
+    chatList[index].content = newContent
+  }
 }
 // #endregion
 
@@ -110,7 +148,18 @@ const registerSocketEvent = () => {
       </div>
       <div class="mt-5" v-if="chatList.length !== 0">
         <ul>
-          <li v-for="(item, i) in chatList" :key="i">{{ item }}</li>
+          <li v-for="(chat, i) in chatList" :key="i">
+            <template v-if="chat.type === 'memo'">
+              {{ chat.user }}さんのメモ [{{ chat.time }}]: {{ chat.content }}
+              <button @click="onEditMemo(i)">編集</button>
+              <button @click="onDeleteMemo(i)">削除</button>
+            </template>
+            <template v-else-if="chat.type === 'publish'">
+              {{ chat.user }}さんの投稿 [{{ chat.time }}]: {{ chat.content }}
+              <button v-if="chat.user === userName" @click="onEditPublish(i)">編集</button>
+              <button v-if="chat.user === userName" @click="onDeletePublish(i)">削除</button>
+            </template>
+          </li>
         </ul>
       </div>
     </div>
@@ -131,9 +180,11 @@ const registerSocketEvent = () => {
   margin-top: 8px;
 }
 
-.item {
-  display: block;
-  white-space: pre-line;
+.chat-item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 8px;
 }
 
 .util-ml-8px {
